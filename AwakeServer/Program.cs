84ec0718806e3ea85a563728f1d53d2422f36185
Loïs <felke2101@gmail.com>
2023-10-22 14:43:04 +0200

    feat: add db connection

--- AwakeServer/Program.cs
+++ AwakeServer/Program.cs
@@ -7,39 +7,53 @@ using System.IO;
 using Awake.CoreServices;
 using Awake.NetworkServices;
 using System.Diagnostics.CodeAnalysis;
+using DotNetEnv;
+using Awake.DatabaseServices;
+using System.Data.Common;
 
 namespace Awake
 {
 	public class AwakeServer {
-		[AllowNull] private static AwakeServer instance;
+		[AllowNull] private static AwakeServer serverInstance;
 		private bool running = false;
 
-		public void Start() {
-			Utils.Log("Server started.");
-			running = true;
-			SocketHandler.Start(); // Hangs until shutdown is requested
-			Utils.Log("Server stopped.");
+		// Ici on crée l'instance de la base de donnée
+        readonly DatabaseAuthentication dbConnect = new();
+
+        public void Start() {
+			try {
+				Utils.Log("Awake App started..");
+				dbConnect.DatabaseConnection(); // TODO -> Fix -> Host can't be null.
+				dbConnect.OpenConnection(); // Ouvrir la connexion à la base de donnée
+				running = true;
+				SocketHandler.Start(); // Hangs until shutdown is requested
+				Utils.Log("Server stopped.");
+
+			} catch(Exception e) {
+				Console.WriteLine("Something wrong.. " + e);
+				dbConnect.CloseConnection();
+			}	
 		}
 
 		public static void Main(string[] args) {
-			instance = new AwakeServer();
+			serverInstance = new AwakeServer();
 
 			Console.CancelKeyPress += delegate(object? sender, ConsoleCancelEventArgs e) {
 				e.Cancel = true;
 				GracefullStop();
 			};
 
-			instance.Start();
+			serverInstance.Start();
 		}
 
 		public static bool isRunning() {
-			return instance.running;
+			return serverInstance.running;
 		}
 		
 		public static void GracefullStop() {
 			Utils.Log("Shutting down gracefully");
 
-			instance.running = false;
+			serverInstance.running = false;
 			SocketHandler.StopServer();
 		}
 	}
