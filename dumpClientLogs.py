a93fb79eace3e43d92092e2f7f875bfd89fe40e5
Cyprien Leschi <cyprien.leschi@gmail.com>
2023-10-23 18:24:44 +0200

    Added dumpClientLogs.py
    Ce programme sert à afficher les logs que le client génère lors de son exécution.

---
+++ dumpClientLogs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+Ce programme sert à afficher les logs que le client génère
+lors de son exécution. Ils sont envoyés via une socket sur
+le port 4444, ce script crée donc un serveur pour recevoir
+ces messages.
+
+TODO: Prendre en charge le format des logs pour extraire
+le message et les afficher correctement.
+"""
+
+import socket
+
+srv = socket.socket()
+srv.bind(('127.0.0.1', 4444))
+srv.listen(1)
+print('Listening on 127.0.0.1:4444')
+cli = None
+
+try:
+    while True:
+        cli, addr = srv.accept()
+        print('[INFO] Client connected.')
+        msg = b'something' # Could be anything other than b''
+
+        while msg!=b'':
+            msg = cli.recv(2048)
+            print(msg.decode())
+    
+        print('\n[INFO] Client disconnected. Waiting for another client...')
+finally:
+    srv.close()
+    if cli != None:
+        cli.close()
